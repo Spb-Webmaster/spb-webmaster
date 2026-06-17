10bfc0e0f3c55fddd7d35724f1e881ca5a2b735a
Axeld <spb-webmaster@mail.ru>
2026-06-17 07:10:27 +0300

    17-iyn-2026-3

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -2,17 +2,32 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\View;
 use Spatie\Sitemap\SitemapGenerator;
 
 class SitemapController extends Controller
 {
-    public function sitemap():void
+    public function sitemap(): void
     {
         $path = public_path('sitemap.xml');
-        SitemapGenerator::create(rtrim(config('app.url'), '/') . '/')->writeToFile($path);
-
+        SitemapGenerator::create(config('app.url'))->writeToFile($path);
+        $this->deduplicateSitemap($path);
     }
 
+    private function deduplicateSitemap(string $path): void
+    {
+        $xml = new \SimpleXMLElement(file_get_contents($path));
+        $seen = [];
 
+        foreach ($xml->url as $url) {
+            $normalized = rtrim((string) $url->loc, '/');
+            if (isset($seen[$normalized])) {
+                $dom = dom_import_simplexml($url);
+                $dom->parentNode->removeChild($dom);
+            } else {
+                $seen[$normalized] = true;
+            }
+        }
+
+        file_put_contents($path, $xml->asXML());
+    }
 }
